99d16b08a604aaa5fe00b80e354a59908dec28f7
Wizqqrl <baban1979molotov@gmail.com>
2025-01-04 22:29:36 +0600

    change password

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
 	cors({
-        origin: "https://portfolio-frontend-dusky.vercel.app", // Укажите правильный URL фронтенда
+        origin: "https://portfolio-frontend-dusky.vercel.app", 
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
